e86dccde66cfa80335fe053b204e857429e4a5cb
Diogo <diogofilipebotelho@gmail.com>
2022-10-14 20:57:35 -0400

    Preparing for deploying frontend app

--- src/SharebnbApi.ts
+++ src/SharebnbApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
-// process.env.REACT_APP_BASE_URL ||
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
 /** API Class.
  *
  * Static class tying together methods used to get/send to to the API.
